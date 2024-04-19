cda9f5b73a63f13aaff51777554dd843af280050
drdaang <99331378+drdaang@users.noreply.github.com>
2024-04-20 01:53:59 +0530

    Updated the model

--- client/src/canvas/Shirt.jsx
+++ client/src/canvas/Shirt.jsx
@@ -1,57 +1,34 @@
-import React from 'react'
-import { easing } from 'maath'
-import { useSnapshot } from 'valtio'
-import { useFrame } from '@react-three/fiber'
-import { Decal, useGLTF, useTexture } from '@react-three/drei';
-import state from '../store';
+import { useEffect, useRef } from "react";
+import { easing } from "maath";
+import { useSnapshot } from "valtio";
+import { useFrame } from "@react-three/fiber";
+import { Decal, useGLTF, useTexture ,useAnimations} from "@react-three/drei";
+
+import state from "../store";
 
 const Shirt = () => {
-    const snap = useSnapshot(state);
-    const { nodes, materials } = useGLTF('/shirt_baked.glb');
-    console.log('nodes:', nodes);
-console.log('materials:', materials);
-    const logoTexture = useTexture(snap.logoDecal);
-    const fullTexture = useTexture(snap.fullDecal);
+ 
+  const snap = useSnapshot(state);
+  const { scene, animations } = useGLTF("tom.glb");
 
-    useFrame((state, delta) => easing.dampC(materials.lambert1.color, snap.color, 0.25, delta));
+  const logoTexture = useTexture(snap.logoDecal);
+  const fullTexture = useTexture(snap.fullDecal);
 
-    const stateString = JSON.stringify(snap);
+
+    
     
 
+  const stateString = JSON.stringify(snap);
+  // const birdRef = useRef();
   return (
-      <group key={stateString} >
-          
-              <mesh
-                  castShadow
-                  geometry={nodes.T_Shirt_male.geometry}
-                  material={materials.lambert1}
-                  material-roughness={1}
-                  dispose={null}
-          >
-              {snap.isFullTexture && (
-                  <Decal
-                      position={[0, 0, 0]}
-                      rotation={[0, 0, 0]}
-                      scale={1}
-                      map={fullTexture}
-                    />
-              )}
-              {snap.isLogoTexture && (
-                  <Decal
-                      position={[0, 0.04, 0.15]}
-                      rotation={[0, 0, 0]}
-                      scale={0.15}
-                      map={logoTexture}
-                    //   map-anisotropy={16}
-                      depthTest={false}
-                      depthWrite={true}
-                    />
-              )}
-
-          </mesh>
-          
+    <group key={stateString}>
+      <mesh  position={[0, -0.6, 0]}
+            rotation={[0, 0, 0]}
+            scale={0.8}>
+        <primitive object={scene} />
+      </mesh>
     </group>
-  )
-}
+    );
+};
 
 export default Shirt;
